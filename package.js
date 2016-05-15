1d98c0767558556117d7fca4df9a0183e6d396c1
romejiang <romejiang@gmail.com>
2016-05-15 18:05:35 +0800

    version 0.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'romejiang:wechat-pay',
-  version: '1.0.0',
+  version: '0.1',
   // Brief, one-line summary of the package.
   summary: 'This is a library to use weixin pay',
   // URL to the Git repository containing the source code for this package.
